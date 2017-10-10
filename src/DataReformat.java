68d647d28cbae090f86c6a4bdab5889d62732302
zhang huaifeng <huaifengzhang@outlook.com>
2017-10-10 20:59:14 +0800

    reformat tracks

--- src/DataReformat.java
+++ src/DataReformat.java
@@ -2,8 +2,9 @@ import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class DataReformat {
-    public static void main(String[] args) {
+    public static void reformatASTS() {
         String[] fileNames = {"1950.data", "1960.data", "1970.data", "1980.data", "1990.data", "2000.data", "2010.data"};
         String parentDir = "E:\\study\\ATST";
         FileWriter fw = null;
@@ -46,6 +47,64 @@ public class DataReformat {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void reformatTRACK() {
+        String parentDir = "E:\\study\\TRACKS\\";
+        String fileName = parentDir + "spline25Aug.txt";
+        FileWriter fw = null;
+        try {
+
+
+            File inputFile = new File(fileName);
+            FileInputStream fis = new FileInputStream(inputFile);
+            InputStreamReader inr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(inr);
+
+            File outFile = new File("trk_output.data");
+            if (outFile.exists()) {
+                outFile.delete();
+            }
+            fw = new FileWriter(outFile, true);
+
+            String regex = "(R[\\d]*)(=)\\[" +
+                    "\\[([\\s\\d\\.\\-]*)\\]\\;" +
+                    "\\[([\\s\\d\\.\\-]*)\\]\\;" +
+                    "\\[([\\s\\d\\.\\-]*)\\]\\;" +
+                    "\\[([\\s\\d\\.\\-]*)\\]\\;" +
+                    "\\[([\\s\\d\\.\\-]*)\\]\\;" +
+                    "\\[([\\s\\d\\.\\-]*)\\]\\]\\;";
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = null;
+            ;
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+
+                matcher = pattern.matcher(line);
+                if (matcher.matches()) {
+                    fw.write(matcher.group(1) + " " + matcher.group(3)  + "\r\n");
+                    fw.write(matcher.group(1) + " " + matcher.group(4)  + "\r\n");
+                    fw.write(matcher.group(1) + " " + matcher.group(5)  + "\r\n");
+                    fw.write(matcher.group(1) + " " + matcher.group(6)  + "\r\n");
+                    fw.write(matcher.group(1) + " " + matcher.group(7)  + "\r\n");
+                    fw.write(matcher.group(1) + " " + matcher.group(8)  + "\r\n");
+                }
+
+
+            }
+
+            fw.flush();
+            fw.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        reformatTRACK();
 
     }
 }
